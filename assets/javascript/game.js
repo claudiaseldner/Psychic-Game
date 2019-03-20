61139b269400cd866c2b9bc8ea09609823d59c14
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-03-19 23:33:45 -0600

    third

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,56 @@
 
-
-
-
-
 // // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+
     var win = 0;
-    var lost  = 0;
-    var opportunities = 9;
-    var possibleLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var usedArray= [];
-    var computerGuess = computerGuess;
+    var losses = 0;
+    var guessesLeft = 9;
+    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var GuessesSoFar = [];
 
-    // Computer Guess
-    computerGuess = possibleLetters[Math.floor(Math.random() * possibleLetters.length)];
-    console.log(computerGuess);
+    var directionsText = document.getElementById('directions-text');
+    var winText = document.getElementById("win-text");
+    var losseText = document.getElementById("losse-text");
+    var guessLeftText = document.getElementById("guess-left-text");
+    var guessSoFarText = document.getElementById("guesses-so-far");
+    var computerChoiceText = document.getElementById("computer-choice");
 
-    document.onkeyup = function (event) {
-        var userGuess = event.key;
+   
+    // function typeLetters () {
+    //   document.getElementById("guesses-so-far").value = document.getElementById()
 
-        if (userGuess === computerGuess) {
-            won++;
-            opportunities = 9;
-            usedArray = [];
-        }
-
-        if (userGuess !== computerGuess) {
-            lost++;
-            opportunities--;
-            usedArray = [];
-
-        }
-
-        }
-    }
-
-// // Create variables that hold references to the places in the HTML where we want to display things.
-//     var userChoice2 = document.getElementById("user-choice");
-//     var computerChoiceText = document.getElementById("computer-choice");
-//     var winText = document.getElementById("win-text");
-//     var losseText = document.getElementById("losse-text");
     
+
+    document.onkeyup = function(event) {
+    var userGuess = (event.key);
+    // var userGuess = guessSoFarText;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // GuessesSoFar.push(document.getElementById("guesses-so-far").value);
+    // guessSoFarText.innerHTML = GuessesSoFar.join();
+   
+    if (userGuess === computerGuess) {
+        win++;
+        guessesLeft = 9;
+        GuessesSoFar.length = 0;
+      } else if (guessesLeft === 0) {
+        losses++;
+        guessesLeft = 9;
+        GuessesSoFar.length = 0;
+      
+
+      } else if (userGuess!== computerGuess) {
+        guessesLeft--;
+      
+      }
     
-  
+      console.log(computerGuess);
+      console.log(userGuess);
 
+   
+    guessSoFarText.innerHTML = "Your Gueses so far: " + GuessesSoFar;
+    GuessesSoFar.push(userGuess);
+    winText.innerHTML = 'Wins: ' + win;
+    losseText.innerHTML = 'Losses: ' + losses;
+    guessLeftText.textContent = "Guesses Left: " + guessesLeft;
+    computerChoiceText.textContent = "" + computerGuess;
+}
 
-//  // This function is run whenever the user presses a key.
-// // // Creates an array that lists out all of the options of letters
-// //     alert("Choose");
-
-//     // var computerGuess =3;
-//     // var possibleLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//     // var userGuess;
-//     // for( var i=0; i < possibleLetters.length; i++) {
-//     //     if (computerGues == guess) {
-//     //         alert("You guess correctly");
-//     //         break;
-        
-//     //     }
-//     //     console.log(i);
-//     }
-// //   // Determines which key was pressed.
-// //   var userGuess = event.key;
-
-// //   // Randomly chooses a choice from the options array. This is the Computer's guess.
-// //   var computerGuess = possibleLetters[Math.floor(Math.random() * possibleLetters.length)];
-
- 
-
-// // }
